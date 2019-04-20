a984873ae0034d6990cf69ef1c7c59b5ff45a6e9
loannpham87 <loannpham87@gmail.com>
2019-04-19 20:30:25 -0700

    progress

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,52 +1,36 @@
-
-  var crystalClicked = 0;
-  var randomNumber = 0;
-  var wins = 0;
-  var losses = 0;
-  var total = 0; //cumulative score of crystalClicked
+var crystalClicked = 0;
+var numberToGuess = 0;
+var wins = 0;
+var losses = 0;
+var total = 0;
 
 function start() {
 
-  var crystalValues = [amethyst, geode, diamond, tourmaline];
 
-  var randomNumber = Math.floor(Math.random() * 100) + 19;
-  $("#random").text(randomNumber)
+  var numberToGuess = Math.floor(Math.random() * 100) + 19;
+  $("#random").text(numberToGuess)
 
   var amethyst = Math.floor(Math.random() * 12) + 1;
-  $("#amethyst").attr("data-id",amethyst);
+  $("#amethyst").attr("data-id", amethyst);
   var geode = Math.floor(Math.random() * 12) + 1;
-  $("#geode").attr("data-id",geode);
+  $("#geode").attr("data-id", geode);
   var diamond = Math.floor(Math.random() * 12) + 1;
-  $("#diamond").attr("data-id",diamond);
+  $("#diamond").attr("data-id", diamond);
   var tourmaline = Math.floor(Math.random() * 12) + 1;
-  $("#tourmaline").attr("data-id",tourmaline);
+  $("#tourmaline").attr("data-id", tourmaline);
 }
 
 start()
 
 
-$(".crystal").on("click", function () {
+$(".crystal").on("click", function gameMode() {
   // console.log(this)
   var value = $(this).attr("data-id")
- crystalClicked += parseInt(value)
-// console.log(value);
+  crystalClicked += parseInt(value)
 
- console.log(crystalClicked);
-
-// total += parseInt(crystalClicked);
-
-
-  $("#total-score").html("<p>" +crystalClicked.toString() + "</p>");
+  $("#total-score").html("<p>" + crystalClicked.toString() + "</p>");
   $("#wins").html("<p>Wins: <br>" + wins.toString() + "</p>");
   $("#losses").html("<p>Losses: <br>" + losses.toString() + "</p>");
-
-  if (value === randomNumber) {
-    wins++;
-  }
-  else wins = wins
-  if (value > randomNumber) {
-    losses++;
-  }
 
 
   //if the total = randomNumber then you win add to the win variable and restart game
@@ -58,7 +42,8 @@ $(".crystal").on("click", function () {
   // if the total > randomNumber you lose add to the lose variable and restart game
   // if the total < keep going 
   //when game restarts, you only need to clear result and crystal values
-// declare/initialize total variable, if you go Over, loss is incremented, however if you match randomNumber, a win is incremented (if/else)
+  // declare/initialize total variable, if you go Over, loss is incremented, however if you match randomNumber, a win is incremented (if/else)
+
 
 });
 
